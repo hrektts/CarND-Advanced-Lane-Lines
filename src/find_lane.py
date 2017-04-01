ff101401120c62c746cda7f2ad5494f4352da2b9
Katsutoshi Horie <mps299792458@gmail.com>
2017-04-02 00:07:40 +0900

    [WIP] work for project_video.mp4

--- src/find_lane.py
+++ src/find_lane.py
@@ -22,15 +22,15 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
 def abs_sobel_thresh(img, orient='x', thresh=(0, 255)):
-    """ Applies Sobel operator and threshold to an image.
+    """ Apply Sobel operator and threshold to an image.
 
     Args:
-        img: A single channel image to be processed.
-        orient: The direction used to calculate the derivative. Takes 'x' or 'y'.
-        thresh: Minimum and maximum thresholds used to binalize the processed image.
+        img: A single channel image to be processed
+        orient: 'x' or 'y': The direction used to calculate the derivative
+        thresh: Minimum and maximum thresholds used to binalize the processed image
 
     Returns:
-        A processed binary image.
+        A processed binary image
 
     """
     # Applies x or y gradient and take the absolute value
@@ -46,15 +46,15 @@ def abs_sobel_thresh(img, orient='x', thresh=(0, 255)):
     return binary_output
 
 def mag_thresh(img, sobel_kernel=3, thresh=(0, 255)):
-    """ Calculates gradient magnitude of an image and applies threshold.
+    """ Calculate gradient magnitude of an image and apply threshold.
 
     Args:
-        img: A single channel image to be processed.
-        sobel_kernel: The kernel size used by Sobel operator.
-        thresh: Minimum and maximum thresholds used to binalize the processed image.
+        img: A single channel image to be processed
+        sobel_kernel: The kernel size used by Sobel operator
+        thresh: Minimum and maximum thresholds used to binalize the processed image
 
     Returns:
-        A processed binary image.
+        A processed binary image
 
     """
     # Take the gradient in x and y separately
@@ -70,15 +70,15 @@ def mag_thresh(img, sobel_kernel=3, thresh=(0, 255)):
     return binary_output
 
 def dir_thresh(img, sobel_kernel=3, thresh=(0, np.pi/2)):
-    """ Thresholds an image for a given range and Sobel kernel
+    """ Threshold an image for a given range and Sobel kernel.
 
     Args:
-        img: A single channel image to be processed.
-        sobel_kernel: The kernel size used by Sobel operator.
-        thresh: Minimum and maximum thresholds used to binalize the processed image.
+        img: A single channel image to be processed
+        sobel_kernel: The kernel size used by Sobel operator
+        thresh: Minimum and maximum thresholds used to binalize the processed image
 
     Returns:
-        A processed binary image.
+        A processed binary image
 
     """
     # Takes the gradient in x and y separately
@@ -93,14 +93,14 @@ def dir_thresh(img, sobel_kernel=3, thresh=(0, np.pi/2)):
     return binary_output
 
 def channel_thresh(img, thresh=(0, 255)):
-    """ Calculates gradient magnitude of an image and applies threshold.
+    """ Calculate gradient magnitude of an image and applies threshold.
 
     Args:
-        img: A single channel image to be processed.
-        thresh: Minimum and maximum thresholds used to binalize the processed image.
+        img: A single channel image to be processed
+        thresh: Minimum and maximum thresholds used to binalize the processed image
 
     Returns:
-        A processed binary image.
+        A processed binary image
 
     """
     # Create a binary mask where mag thresholds are met
@@ -115,10 +115,10 @@ class ImageProcessor:
         """ The initializer
 
         Args:
-            data_dir: A directory used to load and save parameters.
-            cal_img_dir: A directory where calibration images are read from.
-            test_img_dir: A directory where test images are read from.
-            out_img_dir: A directory where processed images are written.
+            data_dir: A directory used to load and save parameters
+            cal_img_dir: A directory where calibration images are read from
+            test_img_dir: A directory where test images are read from
+            out_img_dir: A directory where processed images are written
 
         """
         self.data_dir = data_dir
@@ -145,7 +145,7 @@ class ImageProcessor:
         self.invmtx = None
 
     def read_cal_img_name(self, ext='jpg'):
-        """ Reads all image names in the calibration image directory.
+        """ Read all image names in the calibration image directory.
 
         Args:
             ext: The extention of the images
@@ -157,7 +157,7 @@ class ImageProcessor:
             self.cal_img_names.append(name)
 
     def read_cal_img_shape(self):
-        """ Reads the shape of images.
+        """ Read the shape of images.
         """
         logger.debug('chack the size of calibration images')
 
@@ -175,12 +175,12 @@ class ImageProcessor:
         self.img_shape = max(shapes, key=lambda k: shapes[k])
 
     def calibrate(self, pattern_size=(9, 6), cal_data_file='camera.p'):
-        """ Calibrates camera using chessboard images.
+        """ Calibrate camera using chessboard images.
 
         Args:
-            pattern_size: The number of crossing points of chessboard image.
+            pattern_size: The number of crossing points of chessboard image
                           (x-direction, y-direction)
-            cal_data_file: A file name used to store calibration data.
+            cal_data_file: A file name used to store calibration data
 
         Returns:
             Succeeded (False) or Failed (True)
@@ -213,12 +213,12 @@ class ImageProcessor:
             objpoints.append(objpoint)
             imgpoints.append(corners.reshape(-1, 2))
 
-        ret, mtx, dist, _, _ = cv2.calibrateCamera(objpoints,
-                                                   imgpoints,
-                                                   self.img_shape[0:2],
-                                                   None,
-                                                   None)
-        if ret:
+        succeeded, mtx, dist, _, _ = cv2.calibrateCamera(objpoints,
+                                                         imgpoints,
+                                                         self.img_shape[0:2],
+                                                         None,
+                                                         None)
+        if succeeded:
             self.camera_mtx = mtx
             self.dist_coeffs = dist
 
@@ -234,10 +234,10 @@ class ImageProcessor:
         return True
 
     def load_calibration_data(self, cal_data_file='camera.p'):
-        """ Loads camera calibration data
+        """ Load camera calibration data.
 
         Args:
-            cal_data_file: A file name used to read calibration data.
+            cal_data_file: A file name used to read calibration data
 
         Returns:
             Succeeded (False) or Failed (True)
@@ -261,7 +261,7 @@ class ImageProcessor:
         return True
 
     def undistort(self):
-        """ Undistorts read images and writes them to files.
+        """ Undistort read images and writes them to files.
 
         Returns:
             Succeeded (False) or Failed (True)
@@ -293,7 +293,7 @@ class ImageProcessor:
         return failed
 
     def check_calibration(self, cal_data_file='camera.p'):
-        """ Checks whether the camera calibration has done successfully.
+        """ Check whether the camera calibration has done successfully.
 
         Returns:
             Succeeded (False) or Failed (True)
@@ -304,7 +304,7 @@ class ImageProcessor:
             and self.calibrate()
 
     def has_calibrated(self):
-        """ Checks whether the camera calibration has done or not.
+        """ Check whether the camera calibration has done or not.
 
         Returns:
             Not yet (False) or Done (True)
@@ -325,10 +325,13 @@ class ImageProcessor:
                 warped, binary, undist = self.do_pre_processing(img)
 
                 ftitle, fext = os.path.splitext(os.path.basename(name))
+
                 path = self.out_img_dir + '/' + ftitle + '_warped'+ fext
-                mpimg.imsave(path, warped)
+                out = np.dstack((warped, warped, warped))
+                mpimg.imsave(path, out)
                 path = self.out_img_dir + '/' + ftitle + '_binary'+ fext
-                mpimg.imsave(path, binary)
+                out = np.dstack((binary, binary, binary))
+                mpimg.imsave(path, out)
                 path = self.out_img_dir + '/' + ftitle + '_undist'+ fext
                 mpimg.imsave(path, undist)
 
@@ -356,6 +359,7 @@ class ImageProcessor:
         combined_binary = cv2.bitwise_and(sxbinary, yellow_binary)
         combined_binary = cv2.bitwise_or(combined_binary, s_binary)
         combined_binary = cv2.bitwise_or(combined_binary, sxbinary)
+
         img_size = img.shape[0:2]
         img_size = img_size[::-1]
 
@@ -377,57 +381,43 @@ class ImageProcessor:
         return warped, combined_binary, undist
 
 class Line:
-    """ Line data used by LineDetector
+    """ Line data
     """
     def __init__(self, xm_per_pix=1, ym_per_pix=1):
         """ TODO: Add docstring
         """
-        # was the line detected in the last iteration?
-        self.detected = False
-        # x values of the last n fits of the line
-        self.recent_xfitted = []
-
+        # x values of the last n detection
         self.recent_x = []
+        # y values of the last n detection
         self.recent_y = []
         # average x values of the fitted line over the last n iterations
         self.bestx = None
         # polynomial coefficients averaged over the last n iterations
         self.best_fit = None
+        # polynomial coefficients averaged over the last n iterations
         self.best_fit_for_radius = None
-        # polynomial coefficients for the most recent fit
-        self.current_fit = [np.array([False])]
         # radius of curvature of the line in some units
         self.radius_of_curvature = None
-        # distance in meters of vehicle center from the line
-        self.line_base_pos = None
-        # difference in fit coefficients between last and new fits
-        self.diffs = np.array([0, 0, 0], dtype='float')
-        # x values for detected line pixels
-        self.allx = None
-        # y values for detected line pixels
-        self.ally = None
-
+        # meters per pixel in x dimension
         self.xm_per_pix = xm_per_pix
+        # meters per pixel in y dimension
         self.ym_per_pix = ym_per_pix
-
-    def calc_current_x(self, y):
-        """ TODO: Add docstring
-        """
-        return self.current_fit[0]*y**2 + self.current_fit[1]*y + self.current_fit[2]
 
     def calc_average_x(self, y):
         """ TODO: Add docstring
         """
         return self.best_fit[0]*y**2 + self.best_fit[1]*y + self.best_fit[2]
 
-    def calc_radius(self, y, ym_per_pix=1):
+    def calc_radius(self, y):
         """ TODO: Add docstring
         """
-        return ((1 + (2*self.best_fit_for_radius[0]*y*self.ym_per_pix
-                      + self.best_fit_for_radius[1])**2)**1.5) \
-                      / np.absolute(2*self.best_fit_for_radius[0])
+        self.radius_of_curvature = ((1 + (2*self.best_fit_for_radius[0]*y*self.ym_per_pix
+                                          + self.best_fit_for_radius[1])**2)**1.5) \
+                                          / np.absolute(2*self.best_fit_for_radius[0])
 
     def update(self, current_x, current_y):
+        """ TODO: Add docstring
+        """
         if len(self.recent_x) > 5:
             self.recent_x.pop(0)
         self.recent_x.append(current_x)
@@ -436,8 +426,8 @@ class Line:
             self.recent_y.pop(0)
         self.recent_y.append(current_y)
 
-        all_x = [num for elem in self.recent_x for num in elem]
-        all_y = [num for elem in self.recent_y for num in elem]
+        all_x = np.array([num for elem in self.recent_x for num in elem]).astype(np.float)
+        all_y = np.array([num for elem in self.recent_y for num in elem]).astype(np.float)
 
         self.best_fit = np.polyfit(all_y, all_x, 2)
 
@@ -461,8 +451,10 @@ class LineDetector(ImageProcessor):
 
         """
         super(LineDetector, self).__init__(data_dir, cal_img_dir, test_img_dir, out_img_dir)
-        self.left_line = Line()
-        self.right_line = Line()
+        self.xm_per_pix = 3.7/700
+        self.ym_per_pix = 30/720 # meters per pixel in y dimension
+        self.left_line = Line(self.xm_per_pix, self.ym_per_pix)
+        self.right_line = Line(self.xm_per_pix, self.ym_per_pix)
 
         self.calibrated = False
         self.initial_search = True
@@ -475,9 +467,6 @@ class LineDetector(ImageProcessor):
             self.calibrated = True
 
         binary_warped, _, undist = self.do_pre_processing(img)
-        # Create an output image to draw on and  visualize the result
-        out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
-        window_img = np.zeros_like(out_img)
 
         nonzeroy, nonzerox = binary_warped.nonzero()
         # Set the width of the windows +/- margin
@@ -486,7 +475,6 @@ class LineDetector(ImageProcessor):
         minpix = 50
 
         if self.initial_search:
-            # Assuming you have created a warped binary image called "binary_warped"
             # Take a histogram of the bottom half of the image
             histogram = np.sum(binary_warped[int(binary_warped.shape[0]/2):, :], axis=0)
             # Find the peak of the left and right halves of the histogram
@@ -496,7 +484,7 @@ class LineDetector(ImageProcessor):
             rightx_current = np.argmax(histogram[midpoint:]) + midpoint
 
             # Choose the number of sliding windows
-            nwindows = 9
+            nwindows = 12
             # Set height of windows
             window_height = np.int(binary_warped.shape[0]/nwindows)
 
@@ -562,18 +550,19 @@ class LineDetector(ImageProcessor):
         lefty = nonzeroy[left_lane_inds]
         rightx = nonzerox[right_lane_inds]
         righty = nonzeroy[right_lane_inds]
-        self.left_line.update(leftx, lefty)
-        self.right_line.update(rightx, righty)
 
         # Fit a second order polynomial to each
-        #self.left_line.current_fit = np.polyfit(lefty, leftx, 2)
-        #self.right_line.current_fit = np.polyfit(righty, rightx, 2)
+        self.left_line.update(leftx, lefty)
+        self.right_line.update(rightx, righty)
 
         # Generate x and y values for plotting
         ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0])
         left_fitx = self.left_line.calc_average_x(ploty)
         right_fitx = self.right_line.calc_average_x(ploty)
-        radius = self.left_line.calc_radius(np.max(ploty))
+
+        # Calculate corner radius at the bottom of the image
+        self.left_line.calc_radius(np.max(ploty))
+        self.right_line.calc_radius(np.max(ploty))
 
         warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
         color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
@@ -584,54 +573,32 @@ class LineDetector(ImageProcessor):
         pts = np.hstack((pts_left, pts_right))
 
         # Draw the lane onto the warped blank image
-        cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+        cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
 
-        # Warp the blank back to original image space using inverse perspective matrix (Minv)
+        # Warp the blank back to original image space using inverse perspective matrix
         newwarp = cv2.warpPerspective(color_warp, self.invmtx, (img.shape[1], img.shape[0]))
 
         result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(result, 'Radius of the left line: %9.2f m'
+                    % self.left_line.radius_of_curvature,
+                    (30, 50), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(result, 'Radius of the right line: %8.2f m'
+                    % self.right_line.radius_of_curvature,
+                    (30, 80), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-        '''
-        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-
-        # Generate a polygon to illustrate the search window area
-        # And recast the x and y points into usable format for cv2.fillPoly()
-        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
-        left_line_window2 = np.array([np.flipud(np.transpose(
-            np.vstack([left_fitx+margin, ploty])))])
-        left_line_pts = np.hstack((left_line_window1, left_line_window2))
-        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
-        right_line_window2 = np.array([np.flipud(np.transpose(
-            np.vstack([right_fitx+margin, ploty])))])
-        right_line_pts = np.hstack((right_line_window1, right_line_window2))
-
-
-        # Draw the lane onto the warped blank image
-        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
-        cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
-        #cv2.putText(window_img, str(radius), (100, 100), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255))
-        result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
-        '''
-        '''
-        plt.imshow(result)
-        #plt.plot(left_fitx, ploty, color='yellow')
-        #plt.plot(right_fitx, ploty, color='yellow')
-        plt.xlim(0, 1280)
-        plt.ylim(720, 0)
-        plt.show()
-        '''
         return result
 
+
 def main():
-    opts, _ = getopt.getopt(sys.argv[1:], 'fp', [])
+    opts, _ = getopt.getopt(sys.argv[1:], 'ft', [])
     force = False
-    plot = False
+    test = False
     for opt, _ in opts:
         if opt == '-f':
             force = True
-        if opt == '-p':
-            plot = True
+        if opt == '-t':
+            test = True
 
     detector = LineDetector('../data',
                             '../camera_cal',
@@ -641,20 +608,20 @@ def main():
         detector.calibrate()
         detector.undistort()
 
-    if plot:
+    if test:
         detector.process_test_imgs()
+    else:
+        ## for video processing
+        clip = VideoFileClip('../project_video.mp4')
+        output = '../output_images/project_video.mp4'
 
-    ## for a video
-    #clip = VideoFileClip('../project_video.mp4')
-    #output = '../output_images/project_video.mp4'
+        #clip = VideoFileClip('../challenge_video.mp4')
+        #output = '../output_images/challenge_video.mp4'
 
-    clip = VideoFileClip('../challenge_video.mp4')
-    output = '../output_images/challenge_video.mp4'
+        #clip = VideoFileClip('../harder_challenge_video.mp4')
+        #output = '../output_images/harder_challenge_video.mp4'
 
-    #clip = VideoFileClip('../harder_challenge_video.mp4')
-    #output = '../output_images/harder_challenge_video.mp4'
-
-    clip.fl_image(detector.find_line).write_videofile(output, audio=False)
+        clip.fl_image(detector.find_line).write_videofile(output, audio=False)
 
 if __name__ == '__main__':
     main()
